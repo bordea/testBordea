25843d93ff64e5c586823001c1d2e2521cc5167f
Bordea <test@test.com>
2013-04-13 22:53:12 +0100

    c

--- learnJava/src/week2/GitCommitAndPush.java
+++ learnJava/src/week2/GitCommitAndPush.java
@@ -1,4 +1,11 @@
 package week2;
 
 public class GitCommitAndPush {
+    public static void main(String[] args){
+        int sum = 0;
+        for (int i=0;i<101; i++){
+            sum+=i;
+        }
+        System.out.println("suma primelor 100 de numere este" + sum);
+    }
 }
