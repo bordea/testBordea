d90bb942f44eee66791edaa8ec874dbbcd2c3567
Bordea <test@test.com>
2013-04-14 21:09:45 +0100

    Order of initialization

--- learnJava/src/week2/OrderOfInitialization.java
+++ learnJava/src/week2/OrderOfInitialization.java
@@ -6,8 +6,8 @@ class Window {
 }
 class House {
     Window w1 = new Window(1);                              // Before constructor
-    House() {                                               // Show that we’re in the constructor:
-        System.out.println("House()");
+    House() {
+        System.out.println("House()");                      // Show that we’re in the constructor:
         w3 = new Window(33);                                // Reinitialize w3
     }
     Window w2 = new Window(2);                              // After constructor
