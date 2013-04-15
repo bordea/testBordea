cda492ad9b9fe0dc70077aa8bb9fe9deac9cd296
Bordea <test@test.com>
2013-04-15 14:56:54 +0100

    Fac de unde mi-ai aratat tu ieri , java pentur beginners http://www.homeandlearn.co.uk/java/string_variables.html

--- learnJava/src/week2/FirstProject.java
+++ learnJava/src/week2/FirstProject.java
@@ -1,8 +1,22 @@
 package week2;
 
+import java.util.Scanner;
+
 public class FirstProject{
-    static void StringVariables(String a, String b){
-        System.out.println("The Name is " + a + " and the Surname is " + b);
+    static void StringVariables(String a, String b,char c){
+        System.out.println("The Name is " + a + ", the middle initial is " + c + " and the Surname is " + b);
+    }
+    static void StringVar(){
+        Scanner user_input = new Scanner( System.in );
+        String first_name, family_name, initial, full_name;
+        System.out.print("First Name: ");
+        first_name = user_input.next( );
+        System.out.print("Your initial: ");
+        initial = user_input.next( );
+        System.out.print("Family name: ");
+        family_name = user_input.next( );
+        full_name = first_name + " " + initial + " " + family_name;
+        System.out.println("You are " + full_name);
     }
     public static void main(String args[]){
         int firstN = 10, secondN = 15, totalN;
@@ -10,7 +24,10 @@ public class FirstProject{
         System.out.println("The First number is " + firstN);
         System.out.println("The Second number is " + secondN);
         System.out.println("The Total is " + totalN);
-        String str1="Bordea", str2 = " David";
-        StringVariables(str1, str2);
+        String str1="Bordea", str2 = "David";
+        char c1 = 'C';
+        StringVariables(str1, str2,c1);
+        StringVar();
+
     }
 }
