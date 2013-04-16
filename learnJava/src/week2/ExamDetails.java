88d251f37d65c34249bf1aea3f8471cb0e126316
Bordea <test@test.com>
2013-04-16 16:18:48 +0100

    Classes, global and local scope

---
+++ learnJava/src/week2/ExamDetails.java
@@ -0,0 +1,11 @@
+package week2;
+
+public class ExamDetails {
+    public static void main (String args[]){
+        StudentResults aStudent = new StudentResults();
+        aStudent.Exam_Name = "JAVA Exam";
+        System.out.println(aStudent.Exam_Name);
+
+
+    }
+}
