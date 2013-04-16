593f266d6135b95e89b943260da9521e28bd8c0b
Bordea <test@test.com>
2013-04-17 00:26:20 +0100

    .aaaa

---
+++ learnJava/src/week3/Horse.java
@@ -0,0 +1,5 @@
+package week3;
+
+public class Horse extends Animal {
+     // int speed, String ownerName     method boolean isFast()
+}
---
+++ learnJava/src/week3/MainAnimal.java
@@ -0,0 +1,8 @@
+package week3;
+
+public class MainAnimal {
+
+    public static void main(String[] args){
+
+    }
+}
