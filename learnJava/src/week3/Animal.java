1cbbde30479ee058cae17e732aee2fe0f1859c19
Bordea <test@test.com>
2013-04-17 19:38:47 +0100

    .aaaa

--- learnJava/src/week3/Animal.java
+++ learnJava/src/week3/Animal.java
@@ -8,16 +8,53 @@ public class Animal {   // String nickName, color  ; int averageage ,    , metho
 
     protected int avgAge;
 
-
+    protected String benefit;
 
     //this is the default Animal constructor
     public Animal(){}
 
-    //  this is an Animal constructor used to create Animal objects
+    //  this is an Animal constructor used to create Animal objects with nickname, color and averaAge
     public Animal(String nickname, String color, int avgAge){
         this.nick = nickname;
         this.color = color;
         this.avgAge = avgAge;
     }
 
+    // this is an Animal constructor used to create Animal objects with nickname, color, averaAge and Benefit
+    public Animal(String nickname, String color, int avgAge, String benefit){
+        this.nick = nickname;
+        this.color = color;
+        this.avgAge = avgAge;
+        this.benefit = benefit;
+    }
+
+
+    // this is the getter for the name
+    public  String getNickname(){
+        return nick;
+    }
+
+    // this is the setter for the name
+    public void setNickname(String newNick){
+        this.nick = newNick;
+    }
+
+    // this method tells if an animal is lucky(purple)
+    public boolean isLucky(){
+        boolean bool = false;
+        if (this.color.equals("purple"))
+            bool = true;
+        return bool;
+    }
+
+    // this is the method that returns the whole user details nicely in a String
+    public String toString() {
+        return "The Animal{ " +
+                "Nickname - " + nick +
+                ", color - " + color +
+                ", varsta medie - " + avgAge +
+                ", the benefit - " + benefit +
+                " }"
+                ;
+    }
 }
--- learnJava/src/week3/MainAnimal.java
+++ learnJava/src/week3/MainAnimal.java
@@ -5,5 +5,12 @@ public class MainAnimal {
     public static void main(String[] args){
         Animal horseAnimal = new Animal ("BlackBeauty","purple",15);
         System.out.println(horseAnimal);
+        System.out.println(horseAnimal.getNickname() + " is lucky --> " + horseAnimal.isLucky());
+
+        Animal cowAnimal = new Animal ("Joiana","white",9,"lapte");
+        System.out.println(cowAnimal);
+        System.out.println(cowAnimal.isLucky());
+        System.out.println(cowAnimal.getNickname() + " is lucky --> " + cowAnimal.isLucky());
+
     }
 }
