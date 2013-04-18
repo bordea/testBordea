99a80d08d1517685d5aa7419e6741f0ae40b16c5
Bordea <test@test.com>
2013-04-18 17:49:05 +0100

    .aaaa

---
+++ learnJava/src/Week4/Country.java
@@ -0,0 +1,12 @@
+package Week4;
+
+public class Country {
+    protected String name;
+    protected int code;
+    protected boolean eu;
+
+    public class(){}
+
+
+
+}
---
+++ learnJava/src/Week4/Geography.java
@@ -0,0 +1,6 @@
+package Week4;
+
+public class Geography {
+    Country rom = new Country("Romania",40,false);
+    Country hun = new Country("Hungary",39,true);
+}
