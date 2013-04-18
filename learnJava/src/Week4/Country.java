f0bb28954c34540f02fd696530ffae057a9f1fab
Bordea <test@test.com>
2013-04-18 18:06:00 +0100

    .aaaa

--- learnJava/src/Week4/Country.java
+++ learnJava/src/Week4/Country.java
@@ -5,8 +5,21 @@ public class Country {
     protected int code;
     protected boolean eu;
 
-    public class(){}
+    public class(){};
 
+    public class Country (String name, int code, boolean eu){
+        this.name = name;
+        this.code = code;
+        this.eu = eu;
+    }
 
+    public class Country (String name, boolean eu ){
+        this.name = name;
+        this.eu = eu;
+    }
 
+    public class Country(String name, int code){
+        this.name = name;
+        this.code = code;
+    }
 }
