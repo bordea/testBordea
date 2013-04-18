f54ab9ca463fcb3ef4673bc3905cefb120dc93eb
Bordea <test@test.com>
2013-04-18 19:19:13 +0100

    .aaaa

--- learnJava/src/Week4/Geography.java
+++ learnJava/src/Week4/Geography.java
@@ -4,15 +4,15 @@ public class Geography {
     public static void main(String args[]){
         Country rom = new Country("Romania",40,false);
         Country uk = new Country("UnitedKindgom",39,true);
-        Country tur = new Country("Turkey",false);
-        Country isr = new Country("Israel",52);
+        Country ire = new Country("Ireland",false);
+        Country usa = new Country("UnitatedStates",52);
 
         Country[] array1;
         array1 = new Country[4];
         array1[0]=rom;
         array1[1]=uk;
-        array1[2]=tur;
-        array1[3]=isr;
+        array1[2]=ire;
+        array1[3]=usa;
 
         for (int i=0; i<4;i++){
             System.out.println(array1[i]);
@@ -20,24 +20,26 @@ public class Geography {
 
         County cluj = new County("ClujCounty",264,"CJ");
         County corn = new County("Cornwall",253);
-        County salaj = new County("SalajCounty","SJ");
-        County dolj = new County("DoljCounty",264,"DJ");
+        County ulster = new County("Ulster","ULS");
+        County wdc = new County("Washington",264,"WDC");
 
         County[] array2;
         array2 = new County[4];
         array2[0]=cluj;
         array2[1]=corn;
-        array2[2]=salaj;
-        array2[3]=dolj;
+        array2[2]=ulster;
+        array2[3]=wdc;
 
         for (int i=0; i<4;i++){
             System.out.println(array2[i]);
         }
 
-        City clujNapoca = new City();
-        City municipiuArad = new City();
-        City zalau = new City();
-        City craiova = new City();
+        City clujNapoca = new City(array1,array2);
+        City london = new City();
+        City belfast = new City();
+        City boston = new City();
+
+        System.out.println(clujNapoca);
 
     }
 }
