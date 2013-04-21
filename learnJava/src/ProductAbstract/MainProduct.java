d17efa81d9e173187a66dac96e612aa8c512b605
Bordea <test@test.com>
2013-04-21 15:41:44 +0100

    .aaaa

--- learnJava/src/ProductAbstract/MainProduct.java
+++ learnJava/src/ProductAbstract/MainProduct.java
@@ -44,6 +44,64 @@ public class MainProduct {
 
         cookie.description();
 
+        DelicatFoof cremesh = new DelicatFoof();
+        cremesh.setId("5f6q");
+        cremesh.setPrice(2.54);
+        cremesh.setName("CREMESH");
+        cremesh.setKg(3);
+        cremesh.setAroma("foarte dulce");
+
+        cookie.description();
+
+        DelicatFoof cheesecake = new DelicatFoof();
+        cheesecake.setId("8u9i");
+        cheesecake.setPrice(2.65);
+        cheesecake.setName("CHEESECAKE");
+        cheesecake.setKg(5);
+        cheesecake.setAroma("amar");
+
+        cookie.description();
+
+        DelicatFoof liquerice = new DelicatFoof();
+        liquerice.setId("5t6y");
+        liquerice.setPrice(4);
+        liquerice.setName("LIQUERICE");
+        liquerice.setKg(3);
+        liquerice.setAroma("amar");
+
+        cookie.description();
+
+        DelicatFoof fruitSalad = new DelicatFoof();
+        fruitSalad.setId("8u4h");
+        fruitSalad.setPrice(3.5);
+        fruitSalad.setName("FruitSalad");
+        fruitSalad.setKg(4);
+        fruitSalad.setAroma("dulce");
+
+        cookie.description();
+
+        DelicatFoof cheeeseCrackers = new DelicatFoof();
+        cheeeseCrackers.setId("9i5g");
+        cheeeseCrackers.setPrice(3.05);
+        cheeeseCrackers.setName("CHEESECRACKERS");
+        cheeeseCrackers.setKg(2);
+        cheeeseCrackers.setAroma("amar");
+
+        cookie.description();
+
+        Product[] products = new Product[10];
+        products[0] = fard;
+        products[1] = site;
+        products[2] = boot;
+        products[3] = greenCucumber;
+        products[4] = cookie;
+        products[5] = cremesh;
+        products[6] = cheesecake;
+        products[7] = liquerice;
+        products[8] = fruitSalad;
+        products[9] = cheeeseCrackers;
+
+
 
 
 
