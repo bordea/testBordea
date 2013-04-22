b74c8f259354795f626a39d4ab7e28f2d9a01f10
Bordea <test@test.com>
2013-04-22 21:42:27 +0100

    .aaaa

--- learnJava/src/firstInterface/Dog.java
+++ learnJava/src/firstInterface/Dog.java
@@ -2,13 +2,38 @@ package firstInterface;
 
 interface Pet{
     public void test();
-}
-class Dog implements Pet{
+    public void Eat();
+    public void travel();
+
+    }
+
+public class Dog implements Pet{
     public void test(){
         System.out.println("  |  Interface Method Implemented  |  ");
     }
+
+    @Override
+    public void Eat() {
+        System.out.println("the Dog eats");
+    }
+
+    @Override
+    public void travel() {
+        System.out.println("the Dog travels");
+    }
+
+    public int noOfLegs(){
+        return 4;
+    }
+
     public static void main(String args[]){
         Pet p = new Dog();
         p.test();
+
+        Dog dog1 = new Dog();
+        dog1.Eat();
+        dog1.travel();
+
+
     }
 }
