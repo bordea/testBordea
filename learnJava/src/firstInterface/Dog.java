fd549945d327c162d6b2360b3bf3f9af24a26867
Bordea <test@test.com>
2013-04-22 21:22:09 +0100

    .aaaa

---
+++ learnJava/src/firstInterface/Dog.java
@@ -0,0 +1,14 @@
+package firstInterface;
+
+interface Pet{
+    public void test();
+}
+class Dog implements Pet{
+    public void test(){
+        System.out.println("  |  Interface Method Implemented  |  ");
+    }
+    public static void main(String args[]){
+        Pet p = new Dog();
+        p.test();
+    }
+}
