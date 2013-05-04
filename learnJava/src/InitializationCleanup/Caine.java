792e4f045c205cd56bf21899f4a38ab19d5f0ee4
Bordea <test@test.com>
2013-05-05 00:48:58 +0100

    .

--- learnJava/src/InitializationCleanup/Caine.java
+++ learnJava/src/InitializationCleanup/Caine.java
@@ -1,4 +1,18 @@
+package InitializationCleanup;
 //Exercise 5: (2) Create a class called Dog with an overloaded bark( ) method. This method should be overloaded based
 // on various primitive data types, and print different types of barking, howling, etc., depending on which overloaded
 // version is called. Write a main( ) that calls all the different versions.
-package InitializationCleanup;
+
+public class Caine {
+    public Caine(){}
+
+    public void bark(){
+        //Barking, howling
+    }
+    public static void main(){
+        Caine dog = new Caine();
+        dog.bark("String");
+        dog.bark(3);
+        dog.bark(5.5);
+    }
+}
