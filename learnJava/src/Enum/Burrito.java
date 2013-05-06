b805fc9cb1f750da4b82f2a696b8103ae5ad41b1
Bordea <test@test.com>
2013-05-06 18:06:04 +0100

    .

--- learnJava/src/Enum/Burrito.java
+++ learnJava/src/Enum/Burrito.java
@@ -7,16 +7,16 @@ public class Burrito {
 
     Spiciness degree;
     public Burrito(Spiciness degree) { this.degree = degree;}
-    public void describe() {
-        System.out.print("This burrito is ");
+    public void describe() {                                        // metoda care nu returneaza nimic si printeaza descrierea
+        System.out.print("This burrito is ");                       // unui burrito cu ajutorul uni switch
         switch(degree) {
             case NOT: System.out.println("not spicy at all.");
                 break;
-            case MILD: System.out.println("hot doar cat sa stii ca ii hot ");
+            case MILD: ;//System.out.println("hot doar cat sa stii ca ii hot ");
             case MEDIUM: System.out.println("a little hot.");
                 break;
-            case HOT: System.out.println("iute.");
-            case FLAMING: System.out.println("iti ia foc gura.");
+            case HOT: ;//System.out.println("iute.");
+            case FLAMING: ;//System.out.println("iti ia foc gura.");
             default: System.out.println("maybe too hot.");
         }
     }
@@ -24,8 +24,11 @@ public class Burrito {
         Burrito plain = new Burrito(Spiciness.NOT);
         Burrito greenChile = new Burrito(Spiciness.MEDIUM);
         Burrito jalapeno = new Burrito(Spiciness.HOT);
+        //Burrito ceapa = new Burrito(Spiciness.MILD);
         plain.describe();
         greenChile.describe();
         jalapeno.describe();
+        //ceapa.describe();
+
     }
 }
---
+++ learnJava/src/Enum/Currency.java
@@ -0,0 +1,44 @@
+package Enum;
+// Exercise 21: (1) Create an enum of the least-valuable six types of paper currency. Loop through the values( )
+// and print each value and its ordinal( ).
+
+
+public class Currency {
+    public enum CurrencyType {
+        Pound, Euro, Dollar, Marca, Leu, Rubla
+    }
+
+    CurrencyType value;
+    public Currency(CurrencyType value) {
+        this.value = value;
+    }
+
+    public void describe() {
+        System.out.print("Currency --> ");
+        switch(value) {
+            case Pound: System.out.println("The pounds is the most valuable");
+                break;
+            case Euro: System.out.println("The Euro is valuable");
+                break;
+            case Dollar: System.out.println("The Dollar is valuable enough");
+                break;
+            case Marca: System.out.println("Marca is not not really valuable");
+                break;
+            case Leu: System.out.println("Leu is not valuable");
+                break;
+            case Rubla: System.out.println("Rubla is the least valuable");
+                break;
+            default: System.out.println("TRy another currency from the enum!!!");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Currency british = new Currency(CurrencyType.Pound);
+        Currency european = new Currency(CurrencyType.Euro);
+        Currency american = new Currency(CurrencyType.Dollar);
+        Currency german = new Currency(CurrencyType.Marca);
+        Currency romanian = new Currency(CurrencyType.Leu);
+        Currency russian = new Currency(CurrencyType.Rubla);
+    }
+}
