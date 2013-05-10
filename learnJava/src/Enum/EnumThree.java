2f7f80edb8ed97bae47f395fdf462a6803c6b1c1
Bordea <test@test.com>
2013-05-10 13:35:49 +0100

    .

--- learnJava/src/Enum/EnumThree.java
+++ learnJava/src/Enum/EnumThree.java
@@ -14,7 +14,7 @@ class BillingAccount{
     public static class EnumThree {
         public static void main(String[] args) {
             System.out.println(new BillingAccount());
-            System.out.println(BillingAccount.PaymentTypes.NEW);
+            System.out.println(BillingAccount.PaymentTypes.NEW);     // asa se acceseaza pentru private
         }
     }
 
