9a13c945b4357e720142093c5a0ae9668ddbc294
Bordea <test@test.com>
2013-05-13 21:25:22 +0100

    .

---
+++ learnJava/src/TeorieTutorialeRomana/Introducere.java
@@ -0,0 +1,10 @@
+package TeorieTutorialeRomana;
+
+public class Introducere {
+}
+/*The source file is saved in a file with exension .java
+The file that contains the source code of the class must have the same name as the primary class of the application(the class
+ with the main method).
+At the compilation the apeal is made for the file that contains the main class. A separate file is created by the compiler
+ for each class. The extension for these will be .class and they will be placed in the same directory with the main source.
+To run this manually we can run the command "java AppName" (note that the .class extension is left out) */
