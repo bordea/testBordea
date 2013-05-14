a958350462720429a4df46d175c14d7650f6a5ea
Bordea <test@test.com>
2013-05-14 15:31:18 +0100

    .

--- learnJava/src/TeorieTutorialeRomana/ObiecteSiClase.java
+++ learnJava/src/TeorieTutorialeRomana/ObiecteSiClase.java
@@ -30,3 +30,64 @@ We can force the garbage collection process by executing the gc method from the 
 Before an object is eliminated from the memory gc gets the object to tidy up after itself. After it's used, the object closes the
 files and the sockets used, it destroys the references that it has made towards other objects. This way the gc job is easier.
 */
+/*
+Class Declaration
+[public][abstract][final] class ClassName
+		[extends SuperClassName]
+		[implements Interface1 [, Interface2 ... ]]
+	{
+		// class body
+	}
+The first art of the declaration it's taken by the class modifiers: public, abstract, final
+PUBLIC --> By default, a class can only be used by the classes from the same package with it.(if we don;t specify a package then all
+            the classes inside that directory are considered to be of the same package)
+           Making a class public makes it available to all the classes regardless of their package appartanance.
+ABSTRACT --> A class that has been declared as abstract is like a pattern. We can't make objects of an abstract class so we can't
+                instantiate it. It is simply used as a common pattern for a series of subclasses.
+FInAL --> The final modifiere tells us that the class will not have any subclasses. We can have two purposes when declaring a final class
+ - security : some methods can take as a parameter an object of a class and not a subclass but the type of the object can only be
+    found out at the execution. This way we can eliminate any run time errors that may occur this way.
+ - obiect orientated programming purpose: the idea that a perfect class mustn't have any subclasses
+
+After the declaration we can make the class a subclass or we can make implement one or more interfaces through it.
+
+The body of the class contains:
+    - instance variable and class variable declaration (member variables)
+    - instance and class methods declaration and instance and class methods implementation
+
+Acces specifiers for constructors:
+private --> No other class can instantiate this classes's objects. This kind of class can contain public methods that can create their
+            own objects and make them available to other classes, this way controlling the parameters that create its objects
+protected --> Just its subclasses can create object of the classes's type
+public --> Any class can instantiate this class
+default --> Just the same package classes can instantiate this class
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
