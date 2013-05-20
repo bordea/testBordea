d0cfefeda8ec6370e9841e38dcb964c54c67b5bc
Bordea <test@test.com>
2013-05-20 16:57:30 +0100

    .

---
+++ learnJava/src/TeorieTutorialeRomana/Colectii/Teorie.java
@@ -0,0 +1,28 @@
+package TeorieTutorialeRomana.Colectii;
+
+public class Teorie {
+/*
+A collection is an object that puts together more elements into a single unity. This way we'll have access to types, arrays,
+ tables, etc. The collection are used for the easier storage and usage of data and also for trasmitting the data easier from one
+ method to another.
+
+When we think at collection we think at the following 3 concepts:
+- Interfaces --> abstract data types that describe the collections. These interfaces allow the use of collections independently from
+ the interface.
+- Implementations --> the concrete implementations of the interfaces describe the collections. These classes will represent reusable
+data types.
+- The algorithm --> the methods that run various useful perations like searching, sorting for the objects that implement the
+ interfaces that describe the collection. These algorithms are also called porlymorfics because the same method can be used
+ in different implementations of a collection. We see this way the obvious reusability function.
+
+Advantages, when using collections:
+- the developer has access to a set of data types and algorithms that help with structures and operations used in applications
+    more often so there could be a lot of time saved
+- The effective implementation of collection will shorten the runtime of the program and will also improve its quality. This way we
+ could pay less attention at the way we represent and work with the info and just get on with the solution
+ */
+
+
+
+
+}
