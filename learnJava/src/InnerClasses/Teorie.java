0434d15736682d887f353d523cab10d3df713535
Bordea <test@test.com>
2013-05-21 14:30:40 +0100

    .

---
+++ learnJava/src/InnerClasses/Teorie.java
@@ -0,0 +1,17 @@
+package InnerClasses;
+
+public class Teorie {
+/*
+Inner Classes permit declararea unei clase in interiorul altei clase. Acestea au un scope deoarece putem sa le facem membre ale
+unei alte clase. La fel cum clasele au membri variabile si metod, o clasa poate sa aiba clase membre.
+Avem mai multe modele de clase inner in functie de cum si unde definim inner classes,
+    inclusiv top-level nested class --> inner class care e marcata ca si statica.
+Cele patru topicuri care vor fi parcuerse sunt:
+■ Inner classes
+■ Method-local inner classes
+■ Anonymous inner classes
+■ Static nested classes
+
+
+ */
+}
