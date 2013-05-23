8342927ba240e1572afe7e9fe411e5911c3910e9
Bordea <test@test.com>
2013-05-23 15:04:21 +0100

    .

---
+++ learnJava/src/Generice/Teorie.java
@@ -0,0 +1,24 @@
+package Generice;
+
+public class Teorie {
+/*
+Generice
+
+Chiar daca genericele nu se folosesc doar pentru a face colectiile safe, in general aceasta este intrebuintarea lor practica.
+ Deci chiar daca genericele nu sunt specific legate de colectii, acesta este defapt motivul pentru care s-au introdus genericele in Java.
+Genericele sunt destul de complicate si au chichite asupra carora trebuie insistat.
+Colectiile care nu sunt generice se numesc unsafe collections.
+Pot aparea probleme prin amestecarea genericelor cu non-genericelor.
+
+Colectii prin legacy
+O colectie non-generica poate sa contina orice tip de obiect. O colectie non-generica poate sa contina orice in afara de primitive.
+Cand folosim generice avem optiunea asupra a ceea ce vrem sa punem in colectie si ce rem sa luam din colectie.
+List<String> myList = new ArrayList<String>();
+myList.add("Fred"); // OK, it will hold Strings
+myList.add(new Dog()); // compiler error!!
+Folsind sintaxa generica(sintaxa generica inseamna punerea tipului intre <>) ii spunem compilatorului ca aceasta colectie
+ poate sa contina String.
+ */
+
+
+}
