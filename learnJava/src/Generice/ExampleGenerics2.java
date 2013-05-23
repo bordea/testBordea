fc81489e5f4cbda2821b251a9f31ab0e80662055
Bordea <test@test.com>
2013-05-23 16:10:16 +0100

    .

--- learnJava/src/Generice/ExampleGenerics2.java
+++ learnJava/src/Generice/ExampleGenerics2.java
@@ -1,11 +1,4 @@
 package Generice;
 
-/**
- * Created with IntelliJ IDEA.
- * User: David
- * Date: 5/23/13
- * Time: 4:06 PM
- * To change this template use File | Settings | File Templates.
- */
 public class ExampleGenerics2 {
 }
