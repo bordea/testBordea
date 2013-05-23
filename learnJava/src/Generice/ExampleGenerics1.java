fdfbdc5015ef1cb3ee7e34146030ff637f136868
Bordea <test@test.com>
2013-05-23 16:09:38 +0100

    .

--- learnJava/src/Generice/ExampleGenerics1.java
+++ learnJava/src/Generice/ExampleGenerics1.java
@@ -3,15 +3,25 @@ package Generice;
 public class ExampleGenerics1 {
 
     // generic method printArray
-    public static < E > void printArray( E[] inputArray )
-    {
+    public static < E > void printArray( E[] inputArray ){
+      // 1. Fiecare metoda generica are o sectiune cu tipul parametrului intre <> si este intotdeauna inaintea tipului returnat
+      // 2. Orice sectiune in care se declara parametri ttip va avea , intre parametri. Un parametru tip se mai numeste
+      // variabila tip --> specifica generic type name
+      // 3. Type parametrii pot fi folositi in declaratia tipului returnat si totodata pot sa fie locul de stocare pentru
+      // argumentele transmise metodei generice, care se mai numesc type arguments(argumente tip)
+      // 4. Corpul unei metode este declarat ca orice alta metoda. Type parameters pot reprezenta doar referinte tip si
+      // nicidecum primitive.
         // Display array elements
         for ( E element : inputArray ){
             System.out.println(element );
         }
         System.out.println();
     }
+/*
+    A generic method's body is declared like that of any other method. Note that type parameters can represent only reference
+    types not primitive types (like int, double and char).
 
+  */
     public static void main( String args[] ) {
         // Create arrays of Integer, Double and Character
         Integer[] intArray = { 1, 2, 3, 4, 5 };
