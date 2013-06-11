68023e69edc420d1c99e5a1c68e19870eab4e199
Bordea <test@test.com>
2013-06-11 10:50:26 +0100

    .

---
+++ learnJava/src/operators/Main.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class Main {
+    public static void main (String args[]){
+        System.out.println("Printing through syntax");
+        print("Printing through method call");
+    }
+
+    public static void print(String s){
+        System.out.println(s);
+    }
+}
