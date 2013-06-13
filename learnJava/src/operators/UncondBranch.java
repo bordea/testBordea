442e1072aaecc95ae9a18c9ddf41a87a43f8c194
Bordea <test@test.com>
2013-06-13 19:38:00 +0100

    .

---
+++ learnJava/src/operators/UncondBranch.java
@@ -0,0 +1,34 @@
+package operators;
+
+public class UncondBranch {
+
+    public static void main(String args[]){
+        int[] engineSize =  {1200,2000,1750,1800,1980,2040,3000,5000};
+
+        for (int engine:engineSize){
+            testareMasini(engine);
+        }
+
+        for(int i = 123456789; i > 2000; i=i/13) {
+            if (i%3==0){
+                System.out.println("Numar divizilbil cu 3 --> "+ i);
+            }
+        }
+        for(int i = 123456789; i > 2000; i=i/13) {
+            if (i%3==0){
+                System.out.println("Numar divizilbil cu 3 --> "+ i);
+                break;
+            }
+        }
+    }
+
+    public static String testareMasini(int i){
+        if (i>1800){
+            if (i>5000){
+                return "Masina de mers pe orbita";
+            }
+            else return "Masina de vitezomani";
+        }
+        else return "Masina de melci";
+    }
+}
