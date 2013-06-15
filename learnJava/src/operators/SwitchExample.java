3cb3958f25506de2b5c05d345da7ef4901ca2e65
Bordea <test@test.com>
2013-06-15 10:43:51 +0100

    .

--- learnJava/src/operators/SwitchExample.java
+++ learnJava/src/operators/SwitchExample.java
@@ -17,11 +17,49 @@ public class SwitchExample {
         }
 
         switch(num){
-            case 1 :                // case 1 .. 5 :
-                System.out.println("testing case 1 to 5");
+            case 1 :
+                System.out.println("testing case 1");
                 break;
-            case 2 :                // case 6 .. 10 :
-                System.out.println("testing case 6 to 10");
+            case 2 :
+                System.out.println("testing case 2");
+                break;
+            case 3 :
+                System.out.println("testing case 3");
+                break;
+            case 4 :
+                System.out.println("testing case 4");
+                break;
+            case 5 :
+                System.out.println("testing case 5");
+                break;
+            case 6 :
+                System.out.println("testing case 6");
+                break;
+            case 7 :
+                System.out.println("testing case 7");
+                break;
+            case 8 :
+                System.out.println("testing case 8");
+                break;
+            case 9 :
+                System.out.println("testing case 9");
+                break;
+            case 10 :
+                System.out.println("testing case 10");
+                break;
+        }
+
+        int age = 55;
+
+        switch (age){
+            case 18:
+                System.out.println("Ai 18 ani");
+                break;
+            case 30:
+                System.out.println("Ai 30 ani");
+                break;
+            case 55:
+                System.out.println("Ai 55 ani");
                 break;
         }
     }
