a61ad9030c576b84e41071795a5dd58e80124f8d
Bordea <test@test.com>
2013-06-16 20:20:21 +0100

    .

--- learnJava/src/InitializationAndCleanup/InitializationExample.java
+++ learnJava/src/InitializationAndCleanup/InitializationExample.java
@@ -1,5 +1,7 @@
 package InitializationAndCleanup;
 
+class Depth {}
+
 class Rock {
     Rock() {                                                                    // This is the DEFAULT constructor
         System.out.println("Initializare printr-un contructor simplu ");
@@ -13,6 +15,19 @@ class Rock {
 }
 
 public class InitializationExample {
+    Depth d = new Depth();                        // initializing a non-primitive object
+    // if we don't initialize d before we try and use the compiler will give us an error called an exception
+
+    public class MethodInit {
+        int i = f1();
+        int f1() { return 11; }          // we can call a method to provide an initialization value
+        // int k = g(i);                    // we can't have this before the  int j = f2 () ;
+        int j = f2();
+        int k = g(i);                    // we can't have this before the  int j = f2 () ;
+        int f2() { return 11; }
+        int g(int n) { return n * 10; }
+    }
+
     public static void main(String[] args) {
         for(int i = 0; i < 5; i++){
             new Rock();                 // apelarea unui contructor fara nici un argument, apelarea unui DEFAULT Contructor
@@ -20,5 +35,10 @@ public class InitializationExample {
 
         for(int i = 0; i < 3; i++)      // apelarea unui contructor fara cu un argument
             new Rock(i);
+
+        int i;
+        //i++; // Error --> variable i might not have been initialized
+
+
     }
 }
