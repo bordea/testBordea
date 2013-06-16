660143fcad96cab97ab6305f9365492f50f29826
Bordea <test@test.com>
2013-06-16 17:35:58 +0100

    .

---
+++ learnJava/src/InitializationAndCleanup/PassingThis.java
@@ -0,0 +1,21 @@
+package InitializationAndCleanup;
+
+class Person {
+    public void eat(Apple apple) {
+        Apple peeled = apple.getPeeled();              //
+        System.out.println("Yummy");
+    }
+}
+class Peeler {
+    static Apple peel(Apple apple) {                   //
+        return apple;
+    }
+}
+class Apple {
+    Apple getPeeled() { return Peeler.peel(this); }
+}
+public class PassingThis {
+    public static void main(String[] args) {           //
+        new Person().eat(new Apple());
+    }
+}
